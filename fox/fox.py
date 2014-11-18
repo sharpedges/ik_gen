50e30d1f0bc6a67508ec70321dfe521a6e09c960
sharpedges <kristo.roosileht@nortal.com>
2014-11-18 10:00:19 +0200

    üles

--- fox/fox.py
+++ fox/fox.py
@@ -6,7 +6,8 @@ from datetime import timedelta
 
 date = input("Please enter a date (DD/MM/YYYY): ")
 username = input("Please enter a username: ")
-    
+
+
 def date_to_datetime(date): ## http://stackoverflow.com/questions/17594298/date-time-formats-in-python
     date = datetime.datetime.strptime(date,"%d/%m/%Y")
     return date
@@ -15,14 +16,16 @@ def date_to_timestamp(date): ## http://stackoverflow.com/questions/7873828/how-t
     timestamp = time.mktime(datetime.datetime.strptime(date, "%d/%m/%Y").timetuple())
     timestamp_new = timestamp + 86400 ## 86400 on üks ööpäev
     return timestamp, timestamp_new
-        
 
 conn = sqlite3.connect('main.db')
 c = conn.cursor()
 
 def print_ts():
     sql = ("SELECT body_xml FROM Messages where chatname is '" + username + "' and timestamp between '%s' and '%s'" % (date_to_timestamp(date)))
+    list_sql = []
     for row in c.execute(sql):
-        print (row)
+        print (row[0])
+    conn.close()
 
-print (print_ts())
+if __name__ == '__main__':
+    print (print_ts())
--- ik_gen/isikukood.py
+++ ik_gen/isikukood.py
@@ -1,39 +1,28 @@
-import random
-from datetime import date, timedelta
+import sqlite3
+import datetime
+import time
+import calendar
+from datetime import timedelta
 
-def random_birth_date():
-    days = (date(2099,12,31) - date(1800,1,1)).days
-    birth_date = date(1800,1,1) + timedelta(days = random.randint(0,days))
-    return birth_date    
-
-def generate_birth_date():
-    birth_date = random_birth_date()
-    century = str(birth_date.year)[:2]
-
-    if century == "18":
-        first_nr = random.choice("12")
-    elif century == "19":
-        first_nr = random.choice("34")
-    elif century == "20":
-        first_nr = random.choice("56")
+date = input("Please enter a date (DD/MM/YYYY): ")
+username = input("Please enter a username: ")
     
-    return first_nr + birth_date.strftime("%Y%m%d")[2:]
-def ik():
-    isikukood = generate_birth_date() + format(random.randint(0,999), '03')
-    return isikukood + control_nr(isikukood)
+def date_to_datetime(date): ## http://stackoverflow.com/questions/17594298/date-time-formats-in-python
+    date = datetime.datetime.strptime(date,"%d/%m/%Y")
+    return date
 
-def control_nr(isikukood):
-    for kaalud in [[1,2,3,4,5,6,7,8,9,1],[3,4,5,6,7,8,9,1,2,3]]:
-        jaak_kontroll = jaak(isikukood, kaalud)
-        if jaak_kontroll != 10:
-            return str(jaak_kontroll)
-    return "0"
+def date_to_timestamp(date): ## http://stackoverflow.com/questions/7873828/how-to-convert-date-to-timestamp-using-python
+    timestamp = time.mktime(datetime.datetime.strptime(date, "%d/%m/%Y").timetuple())
+    timestamp_new = timestamp + 86400 ## 86400 on üks ööpäev
+    return timestamp, timestamp_new
+        
 
-def jaak(isikukood, kaalud):
-    summa = 0
-    for i in range(len(isikukood)):
-        summa += kaalud[i] + int(isikukood[i])
-    return summa % 11
+conn = sqlite3.connect('main.db')
+c = conn.cursor()
 
-if __name__ == "__main__":
-    print(ik())
+def print_ts():
+    sql = ("SELECT body_xml FROM Messages where chatname is '" + username + "' and timestamp between '%s' and '%s'" % (date_to_timestamp(date)))
+    for row in c.execute(sql):
+        print (row)
+
+print (print_ts())
